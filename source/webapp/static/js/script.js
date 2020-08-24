5ac09df9162c7f79e81fc5f8f002890cbd179f09
Evgenii <neroznik@yandex.ru>
2020-08-24 13:26:58 +0600

    Изменил внешний вид

--- source/webapp/static/js/script.js
+++ source/webapp/static/js/script.js
@@ -1,4 +1,9 @@
-function justdoit(){
-document.form1.select1.selectedIndex=2;
-document.form1.select1.onchange();
+$(function (){
+var itemCount = $('.wrap .item').length;
+var maxH = 0;
+for (i=0; i<itemCount; i++) {
+	var itemH = $('.wrap .item').eq(i).height();
+  if (itemH > maxH) {maxH = itemH;}
 }
+$('.wrap .item').css('height', maxH);
+});
