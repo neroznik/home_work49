c35271ef8d2d8f5b52f9837a01a4fb9c0253d92e
Evgenii <neroznik@yandex.ru>
2020-09-14 15:25:37 +0600

    Добавлена модель Profile

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Profile(models.Model):
+    avatar = models.ImageField(null=True, blank=True, upload_to='user_pics', verbose_name='Аватар')
+    user = models.OneToOneField(get_user_model(), related_name='profile', on_delete=models.CASCADE, verbose_name='Пользователь')
+    about = models.TextField(max_length=4000, null=True,blank=True, verbose_name='О себе')
+    git_url = models.URLField(max_length=200, null=True,blank=True, verbose_name= 'GitHub link')
+
+
+    def __str__(self):
+        return self.user.get_full_name() + "'s Profile"
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
